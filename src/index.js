3f393538e22ca62540915f2c3dec461b18d42291
masha <deathcore0001@gmail.com>
2021-01-20 14:45:29 +0500

    last block anf slick

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import "./scss/main.scss";
+import $ from 'jquery';
 import 'bootstrap';
-import 'slick-carousel';
+import Swiper , { Navigation, Pagination } from 'swiper';
+Swiper.use([Navigation, Pagination]);
+
+
+$(document).ready(function () {
+
+    let mySwiper = new Swiper('.swiper-container', {
+        direction: 'horizontal',
+        slidesPerView: 4,
+        spaceBetween: 30,
+        loop: true,
+        centeredSlides: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+        navigation: {
+            nextEl: '.next',
+            prevEl: '.prev',
+        },
+
+    });
+})
+
+
